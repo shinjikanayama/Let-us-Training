8751eafa36acb0115967f394c492b7ee9649c244
shinjikanayama <sinzi.1202@gmail.com>
2023-10-16 11:34:09 +0000

    [データの修正]

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,11 +12,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def create
     ActiveRecord::Base.transaction do
       super
-      Genre.create(name: "胸トレ", user_id: current_user.id)
-      Genre.create(name: "背中トレ", user_id: current_user.id)
-      Genre.create(name: "足トレ", user_id: current_user.id)
-      Genre.create(name: "肩トレ", user_id: current_user.id)
-      Genre.create(name: "その他", user_id: current_user.id)
+      if resource.persisted?
+        Genre.create(name: "胸トレ", user_id: current_user.id)
+        Genre.create(name: "背中トレ", user_id: current_user.id)
+        Genre.create(name: "足トレ", user_id: current_user.id)
+        Genre.create(name: "肩トレ", user_id: current_user.id)
+        Genre.create(name: "その他", user_id: current_user.id)
+      else
+         # ユーザーの新規登録が失敗した場合
+        clean_up_passwords resource
+        set_minimum_password_length
+      end
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'dotenv'
+Dotenv.load
+
 User.find_or_create_by!(email: "zzz@zzz") do |user|
   user.name = "admin"
   user.password = ENV['DB_PASSWORD_ADMIN']
